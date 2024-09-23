2359a843edcc25a14971d7ab0225e45167ac75a0
vincentamu <123021332+vincentamu@users.noreply.github.com>
2024-09-23 17:25:13 -0500

    Update students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  skip_before_action :student_login!, only: [:new, :create]
   before_action :set_student, only: %i[show edit update destroy]
 
   def index
