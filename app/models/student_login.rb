7da8373158fbdb4f5d4f54fad4c78e9b5785203d
vincentamu <123021332+vincentamu@users.noreply.github.com>
2024-09-23 16:28:47 -0500

    Update student_login.rb

--- app/models/student_login.rb
+++ app/models/student_login.rb
@@ -12,7 +12,6 @@ class StudentLogin < ApplicationRecord
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     first_name, last_name = full_name.split(' ', 2) 
     student_login = create_with(full_name:, avatar_url:).find_or_create_by!(email:)
-    student_login.build_student(email: email, first_name: first_name, last_name:last_name) if student_login.student.blank?
     student_login
   end
 end
