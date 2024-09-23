06332d3c14764c2a9a8ebcf473da7a7d5620289c
vincentamu <123021332+vincentamu@users.noreply.github.com>
2024-09-23 16:15:30 -0500

    Update student_login.rb

--- app/models/student_login.rb
+++ app/models/student_login.rb
@@ -5,7 +5,13 @@ class StudentLogin < ApplicationRecord
 
   validates :uid, presence: true
 
+  # Add association to Student
+  belongs_to :student, optional: true # optional: true if student might not be present initially
+
+  # Define the method to create or find a student
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    create_with(uid:, full_name:, avatar_url:).find_or_create_by!(email:)
+    student_login = create_with(full_name:, avatar_url:).find_or_create_by!(email:)
+    student_login.build_student(email: email, full_name: full_name) if student_login.student.blank?
+    student_login
   end
 end
